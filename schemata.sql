0269fba8026ba3f19cefce52b118b26b45a27860
git@rice.bootcampcontent.com:Rice-Coding-Bootcamp/RICEHOU201906DATA1.git <47514609+rgalindo125@users.noreply.github.com>
2019-08-09 16:00:24 -0500

    Add files via upload

---
+++ schemata.sql
@@ -0,0 +1,60 @@
+CREATE TABLE employees
+(
+    emp_no INT NOT NULL,
+    birth_date DATE NOT NULL,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    gender VARCHAR(100) NOT NULL,
+    hire_date DATE NOT NULL,
+    PRIMARY KEY (emp_no)
+);
+
+CREATE TABLE departments
+(
+    dept_no VARCHAR(100) NOT NULL,
+    dept_name VARCHAR(100) NOT NULL,
+    PRIMARY KEY (dept_no),
+    UNIQUE   	(dept_name)
+);
+
+CREATE TABLE dept_manager
+(
+    dept_no VARCHAR(100) NOT NULL,
+    emp_no INT NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
+    FOREIGN KEY (emp_no)  REFERENCES employees (emp_no),
+    FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+    PRIMARY KEY (emp_no,dept_no)
+);
+
+CREATE TABLE dept_emp
+(
+    emp_no INT NOT NULL,
+    dept_no VARCHAR(100) NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
+    FOREIGN KEY (emp_no)  REFERENCES employees   (emp_no),
+    FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+    PRIMARY KEY (emp_no,dept_no)
+);
+
+CREATE TABLE titles
+(
+    emp_no INT NOT NULL,
+    title VARCHAR(100) NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE,
+    FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+    PRIMARY KEY (emp_no,title, from_date)
+);
+
+CREATE TABLE salaries
+(
+    emp_no INT NOT NULL,
+    salary INT NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
+    FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+    PRIMARY KEY (emp_no, from_date)
+);
